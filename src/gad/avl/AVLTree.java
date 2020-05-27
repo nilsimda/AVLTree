130adc4ef2f2357130c314aa042e0baf72c2617a
JoejoeSt <38322605+JoejoeSt@users.noreply.github.com>
2020-05-27 11:00:07 +0200

    Not empty

---
+++ src/gad/avl/AVLTree.java
@@ -0,0 +1,46 @@
+package gad.avl;
+
+public class AVLTree {
+    private AVLTreeNode root = null;
+
+    public AVLTree() {
+    }
+    
+    public AVLTreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(AVLTreeNode root) {
+        this.root = root;
+    }
+
+    public boolean validAVL() {
+        return false;
+    }
+
+    public void insert(int key) {
+    }
+
+    public boolean find(int key) {
+        return false;
+    }
+
+    /**
+     * Diese Methode wandelt den Baum in das Graphviz-Format um.
+     *
+     * @return der Baum im Graphiz-Format
+     */
+    private String dot() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("digraph {\n");
+        if (root != null)
+            root.dot(sb);
+        sb.append("}");
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return dot();
+    }
+}
